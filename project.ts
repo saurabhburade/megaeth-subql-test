ed148cadfcda8980aebcfa235da87e52a7b7c5f5
saurabhburade <sburade3@gmail.com>
2025-07-26 03:02:43 +0530

    chore: track pools every 1800 blks

--- project.ts
+++ project.ts
@@ -95,7 +95,7 @@ const project: EthereumProject = {
             kind: EthereumHandlerKind.Block,
             handler: "handleBlockForPools",
             filter: {
-              modulo: 300,
+              modulo: 1800,
             },
           },
           {
